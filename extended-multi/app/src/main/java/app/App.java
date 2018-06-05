9477df5e0c02c6168706f7f39e911c26fde8a249
Petro Karabyn <karabyn@ucu.edu.ua>
2018-06-05 11:46:26 +0300

    Introduce multi module dependency management

--- extended-multi/app/src/main/java/app/App.java
+++ extended-multi/app/src/main/java/app/App.java
@@ -1,11 +1,13 @@
 package app;
 
 import config.ConfigService;
+import org.apache.commons.lang3.StringUtils;
 import util.Util;
 
 public class App {
 
     public String greet(String name) {
+        StringUtils.trim("dummy");
         return Util.join("Hello ", name);
     }
 
